e7c2f2997171d288272104d03b7a0de13b16744b
ElWas1 <tinoxdwtf@gmail.com>
2023-03-07 10:45:31 -0300

    Añade dinamismo a las cards y pequeñas mejoras visuales y de diseño

---
+++ scripts/past_events.js
@@ -0,0 +1,18 @@
+const cardElements = document.getElementById("cards-container");
+let cards = '';
+
+for (let i = 0; i < data.events.length; i++) {
+    if (data['events'][i].date.includes('2021')) {
+        cards += `<div class="card m-4">
+        <img src="${data['events'][i].image}" class="card-img-top" alt="${data['events'][i].name}">
+        <div class="card-body d-flex">
+            <h5 class="card-title">${data['events'][i].name}</h5>
+            <p class="card-text"> ${data['events'][i].description} </p>
+            <p>Price: $${data['events'][i].price}</p>
+            <a href="./details.html" class="btn btn-primary">More info</a>
+        </div>
+    </div>`
+    }
+};
+
+cardElements.innerHTML = cards;
